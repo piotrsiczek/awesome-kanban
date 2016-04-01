72b4083c5c957f07750331631f60ba52423f78c8
Piotr Siczek <psc@touk.pl>
2016-04-01 17:51:54 +0200

    web sceleton

---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webApiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webApiApp
+ */
+angular.module('webApiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
